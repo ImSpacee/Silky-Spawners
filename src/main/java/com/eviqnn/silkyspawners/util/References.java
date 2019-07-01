2a7d24aafe792966f4ce24dbd53069c21049eb4f
Codetoil <ianthisawesomee@gmail.com>
2019-07-01 10:17:51 -0400

    It now silk touches :D

--- src/main/java/com/eviqnn/silkyspawners/util/References.java
+++ src/main/java/com/eviqnn/silkyspawners/util/References.java
@@ -2,11 +2,9 @@ package com.eviqnn.silkyspawners.util;
 
 public class References {
 	
-	public static final String MOD_ID = "silk";
+	public static final String MOD_ID = "silkyspawners";
 	public static final String NAME = "Silky Spawners";
-	public static final String VERSION = "1.1";
-	
-	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String VERSION = "1.1.1";
 	
 	public static final String CLIENT_PROXY_CLASS = "com.eviqnn.silkyspawners.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.eviqnn.silkyspawners.proxy.CommonProxy";
