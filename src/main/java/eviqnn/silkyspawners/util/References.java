14a5a43798d3929da8d4551c5975bbcfa69f068d
Codetoil <ianthisawesomee@gmail.com>
2019-07-19 21:58:21 -0400

    Update to 1.2

--- src/main/java/eviqnn/silkyspawners/util/References.java
+++ src/main/java/eviqnn/silkyspawners/util/References.java
@@ -4,5 +4,5 @@ public class References
 {
 	public static final String MOD_ID = "silkyspawners";
 	public static final String NAME = "Silky Spawners";
-	public static final String VERSION = "1.2 Prerelease PI";
+	public static final String VERSION = "1.2";
 }
